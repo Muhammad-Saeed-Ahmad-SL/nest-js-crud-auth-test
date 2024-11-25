42125b9b5b0e344188ec4114b9263d6b2742f095
Muhammad-Saeed-Ahmad-SL <saeed.ahmad@solutionsloft.com>
2024-11-25 19:39:12 +0500

    Book CRUD, Authentication, Authorization

---
+++ src/book/schemas/book.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { User } from 'src/auth/schemas/user.schema';
+
+export enum Category {
+  ADVENTURE = 'Adventure',
+  CLASSICS = 'Classics',
+  FANTASY = 'Fantasy',
+  CRIME = 'Crime',
+}
+@Schema({
+  timestamps: true,
+})
+export class Book {
+  @Prop()
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  author: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  category: Category;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  user: User;
+}
+export const BookSchema = SchemaFactory.createForClass(Book);
