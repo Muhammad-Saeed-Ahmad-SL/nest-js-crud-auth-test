51b91aa19b1241fa12d9514a8e710f4175951ce3
Muhammad-Saeed-Ahmad-SL <saeed.ahmad@solutionsloft.com>
2024-12-02 12:15:39 +0500

    write e2e test

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
