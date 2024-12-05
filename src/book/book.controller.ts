f7c59792ee7358156e11b63b29cf3f097d0fd239
Muhammad-Saeed-Ahmad-SL <saeed.ahmad@solutionsloft.com>
2024-12-05 11:54:49 +0500

    handle file with multer and api security helmet, cors, throttler api limiting

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -3,12 +3,16 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
+  ParseFilePipeBuilder,
   Post,
   Put,
   Query,
   Req,
+  UploadedFiles,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
@@ -20,11 +24,15 @@ import { AuthGuard } from '@nestjs/passport';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { Role } from '../auth/enums/role.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
+  // @SkipThrottle() // To skip this route from throttling
+  @Throttle({ default: { limit: 1, ttl: 2000 } }) // To throttle this route
   @Get()
   @Roles(Role.Moderator, Role.Admin)
   @UseGuards(AuthGuard(), RolesGuard)
@@ -56,5 +64,28 @@ export class BookController {
   @Delete(':id')
   async deleteBook(@Param('id') id: string): Promise<{ deleted: boolean }> {
     return this.bookService.deleteById(id);
+  }
+
+  @Put('upload/:id')
+  @UseGuards(AuthGuard())
+  @UseInterceptors(FilesInterceptor('files'))
+  async uploadImages(
+    @Param('id') id: string,
+    @UploadedFiles(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /(jpg|jpeg|png)$/,
+        })
+        .addMaxSizeValidator({
+          maxSize: 10 * 10000,
+          message: 'File size must be less than 10kb',
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    files: Array<Express.Multer.File>,
+  ) {
+    return this.bookService.uploadImages(id, files);
   }
 }
